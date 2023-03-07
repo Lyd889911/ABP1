aead0fe3715e6a00b8fc6042a2836e676f640ad8
lyd <206977438@qq.com>
2023-03-08 02:52:23 +0800

    1

---
+++ src/Acme.BookStore.Application.Contracts/Books/CreateUpdateBookDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Acme.BookStore.Books
+{
+    /// <summary>
+    /// 创建图书时和请求图书的dto
+    /// </summary>
+    public class CreateUpdateBookDto
+    {
+        [Required]
+        [StringLength(128)]
+        public string Name { get; set; }
+
+        [Required]
+        public BookType Type { get; set; } = BookType.Undefined;
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime PublishDate { get; set; } = DateTime.Now;
+
+        [Required]
+        public float Price { get; set; }
+    }
+}
